e1fda028f2bdb954714289f109b24dbec706f988
Alex Hooper <alexanderghooper@gmail.com>
2019-11-04 21:52:09 +0000

    store setup

--- client/src/store/index.tsx
+++ client/src/store/index.tsx
@@ -1,14 +1,27 @@
 import React, { useState, useEffect } from 'react';
 
-export const contextStore: object = React.createContext({
-
+export const contextStore = React.createContext({
+  test: false,
+  setTest: () => { }
 });
 
-export default function StateProvider() {
+export default function StateProvider({ children }) {
 
+  const setTest = () => {
+    setStore({ ...store, test: true })
+  }
 
+  const initialState: object = {
+    test: false,
+    setTest: setTest
+  };
+
+  const [store, setStore] = useState(initialState);
 
   return (
+    <contextStore.Provider value={store}>
+      {children}
+    </contextStore.Provider>
 
   );
 };
